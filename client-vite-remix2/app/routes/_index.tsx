9bed42486ab97c47d15a1d2e506be21540b714d8
bronifty <bronifty@gmail.com>
2024-03-13 23:15:36 -0400

    this

---
+++ client-vite-remix2/app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Index</h1>
+      <Link to="hello">hello </Link>
+    </div>
+  );
+}
